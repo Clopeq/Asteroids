cbc0c8982cb64923512a0c46fc2975746e539aac
Clopeq <seba.krol07@gmail.com>
2025-08-31 12:39:56 +0200

    delta time and limit fps

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +15,7 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
